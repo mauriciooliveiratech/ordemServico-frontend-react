76a966f95e8ee2c7c37d0b1b8cc66c68f6a79981
MAURICIO DE AZEVEDO OLIVEIRA <mauriciooliveira@MacBook-Pro-de-MAURICIO.local>
2026-02-14 12:35:06 -0300

    Fix: Use Railway API URL

--- src/Services/api.ts
+++ src/Services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8080", // backend Spring Boot
+  baseURL: "http://https://ordem-servico-frontend.vercel.app:8080", // backend Spring Boot
 });
 
 
