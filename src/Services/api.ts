92749064c5553b1dcd14c94eee520aeb12359f13
MAURICIO DE AZEVEDO OLIVEIRA <mauriciooliveira@MacBook-Pro-de-MAURICIO.local>
2026-02-17 00:06:02 -0300

    api 0005

--- src/Services/api.ts
+++ src/Services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://ordem-servico-frontend.vercel.app", // backend Spring Boot
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 
