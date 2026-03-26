f973bb483a977a7e7e0ad8c96eb4a4ea6bc11a57
MAURICIO DE AZEVEDO OLIVEIRA <mauriciooliveira@MacBook-Pro-de-MAURICIO.local>
2026-03-25 21:27:11 -0300

    atualizacao

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,14 +3,12 @@ import { Dashboard } from "../pages/Dashboard/Dashboard";
 import { MarcaPage } from "../pages/Cadastros/MarcaPage";
 import { ModeloPage } from "../pages/Cadastros/ModeloPage";
 import { ServicoPage } from "../pages/Cadastros/ServicoPage";
-import Login from "../pages/login/login";
+
 
 export default function RoutesApp() {
   return (
     <Routes>
-      <Route path="/login" element={<Login onLogin={function (): void {
-        throw new Error("Function not implemented.");
-      } } />} />
+    
 
       <Route path="/" element={<Dashboard />} />
       <Route path="/os" element={<Dashboard />} />
