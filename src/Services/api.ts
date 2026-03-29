e13b3b7bf5d479c2c8bd419b5416a69678b8823f
Mauricio Oliveira Dev <85310953+mauriciooliveiratech@users.noreply.github.com>
2026-03-29 09:34:23 -0300

    Refactor API baseURL configuration

--- src/Services/api.ts
+++ src/Services/api.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
 
+const baseURL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api";
+
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL + "/api",
+  baseURL,
   headers: {
     "Content-Type": "application/json",
   },
 });
-
 
 
