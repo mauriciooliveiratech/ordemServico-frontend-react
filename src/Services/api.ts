1513ba2590ac8866e3c70c330e9aede2dfed527f
Mauricio Oliveira Dev <85310953+mauriciooliveiratech@users.noreply.github.com>
2026-03-29 09:50:01 -0300

    Update api.ts

--- src/Services/api.ts
+++ src/Services/api.ts
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-const baseURL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api";
-
 export const api = axios.create({
-  baseURL,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
