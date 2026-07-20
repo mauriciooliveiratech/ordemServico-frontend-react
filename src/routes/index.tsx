c3c7185391d0d6dbd7eb2b85106d24b1a18f8904
Mauricio Oliveira Dev <85310953+mauriciooliveiratech@users.noreply.github.com>
2026-07-19 21:15:38 -0300

    Ativa nova tela do PDV

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,9 @@
 import { Route, Routes } from "react-router-dom";
-import { Overview, Pos, Reports } from "../pages/Operations";
+import { Overview, Reports } from "../pages/Operations";
 import { Usuarios } from "../pages/Usuarios";
 import { useAuth } from "../contexts/useAuth";
 import {OrdemServicoReal} from "../pages/OrdemServicoReal";
 import {CadastrosReal} from "../pages/Cadastros/CadastrosReal";
 import {InventoryReal} from "../pages/InventoryReal";
-export default function RoutesApp(){const {usuario}=useAuth(),admin=usuario?.papel==="ADMIN"||usuario?.papel==="PROPRIETARIO";return <Routes><Route path="/" element={<Overview/>}/><Route path="/ordens" element={<OrdemServicoReal/>}/><Route path="/os" element={<OrdemServicoReal/>}/><Route path="/pdv" element={<Pos/>}/><Route path="/insumos" element={<InventoryReal/>}/><Route path="/cadastros" element={<CadastrosReal/>}/>{admin&&<Route path="/relatorios" element={<Reports/>}/>} {admin&&<Route path="/usuarios" element={<Usuarios/>}/>}<Route path="*" element={<Overview/>}/></Routes> }
+import {PdvReal} from "../pages/PdvReal";
+export default function RoutesApp(){const {usuario}=useAuth(),admin=usuario?.papel==="ADMIN"||usuario?.papel==="PROPRIETARIO";return <Routes><Route path="/" element={<Overview/>}/><Route path="/ordens" element={<OrdemServicoReal/>}/><Route path="/os" element={<OrdemServicoReal/>}/><Route path="/pdv" element={<PdvReal/>}/><Route path="/insumos" element={<InventoryReal/>}/><Route path="/cadastros" element={<CadastrosReal/>}/>{admin&&<Route path="/relatorios" element={<Reports/>}/>} {admin&&<Route path="/usuarios" element={<Usuarios/>}/>}<Route path="*" element={<Overview/>}/></Routes> }
